20b696b87a0783f4d6579ad64039afcc25cea063
omeraba <omeraba93@gmail.com>
2022-04-25 13:28:08 +0300

    sending github

---
+++ custom_addons/om_hospital/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+
+{
+    'name': 'Hospital management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'author': 'Omer ABA',
+    'sequence': -100,
+    'summary': 'Hospital management system',
+    'description': """ Hospital management system """,
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/menu.xml',
+        'views/patient_view.xml',
+    ],
+    'demo': [],
+    'application': 'True',
+    'installable': True,
+    'auto_install': False,
+    'assets': {},
+    'licence': 'LPGL-3',
+}
