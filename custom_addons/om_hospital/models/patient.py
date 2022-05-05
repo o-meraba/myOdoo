aa8668e7fcb56860637f62373008744faf34ba1f
omeraba <omeraba93@gmail.com>
2022-05-05 15:08:08 +0300

    added func for automatic compute age from birthday

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -1,5 +1,5 @@
 from odoo import api, fields, models
-
+from datetime import date
 
 class HospitalPatient(models.Model):
 
@@ -9,8 +9,18 @@ class HospitalPatient(models.Model):
     _order = 'name desc'
 
     name = fields.Char(string="Name", tracking=True)
-    age = fields.Integer(string="Age")
+    date_of_birth = fields.Date("Date Of Birth")
+    age = fields.Integer(string="Age", compute='_compute_age')  # if there is compute feature, it won't be stored in db, if you want, you can add store=True
     ref = fields.Char(string="Reference", default='HB00')
     gender = fields.Selection([('male', "Male"), ('female', 'Female')], string='Gender', default='male')
     active = fields.Boolean(string="Active", default=True)
     note = fields.Text(string="Description", tracking=True)
+
+    @api.depends('date_of_birth')  # for compute before clicking save button
+    def _compute_age(self):
+        for rec in self:
+            today = date.today()
+            if rec.date_of_birth:
+                rec.age = today.year - rec.date_of_birth.year
+            else:
+                rec.age = 0
