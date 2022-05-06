158797fafd96caca48e87c2949c7658d7344eb9c
omeraba <omeraba93@gmail.com>
2022-05-06 19:18:11 +0300

    added priority widget

--- custom_addons/om_hospital/models/appointment.py
+++ custom_addons/om_hospital/models/appointment.py
@@ -14,6 +14,11 @@ class HospitalAppointment(models.Model):
     gender = fields.Selection(related='patient_id.gender')
     ref = fields.Char(string="Reference")
     prescription = fields.Html(string='Prescription')
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Very High')], string="Priority")
 
     @api.onchange('patient_id')
     def onchange_patient_id(self):
