c3d85294a991f192363f662a682006a68d4c7951
omeraba <omeraba93@gmail.com>
2022-05-18 21:11:11 +0300

    added many2many feature

--- custom_addons/om_hospital/models/patient.py
+++ custom_addons/om_hospital/models/patient.py
@@ -17,6 +17,7 @@ class HospitalPatient(models.Model):
     note = fields.Text(string="Description", tracking=True)
     appointment_id = fields.Many2one('hospital.appointment', string='Appointments')
     image = fields.Image(string="Image")
+    tag_ids = fields.Many2many('patient.tag', string="Tags")
 
     @api.depends('date_of_birth')  # for compute before clicking save button
     def _compute_age(self):
